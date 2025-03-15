a50efb20175cab27c6685c61524135c45d5bda3b
Baklazan-123 <michal.osytek@interia.pl>
2025-03-15 15:56:24 +0100

    Wyswietlenie weza.

--- projekt.py
+++ projekt.py
@@ -2,6 +2,8 @@ import pygame
 import random
 import time
 from jablko import Jablko
+from kierunek import Kierunek
+from waz import Waz
 
 SZEROKOSC_EKRANU = 800
 WYSOKOSC_EKRANU = 608
@@ -21,6 +23,11 @@ pygame.init()
 ekran = pygame.display.set_mode([SZEROKOSC_EKRANU, WYSOKOSC_EKRANU])
 zegar = pygame.time.Clock()
 
+waz = Waz()
+
+PORUSZ_WEZEM = pygame.USEREVENT + 1
+pygame.time.set_timer(PORUSZ_WEZEM, 200)
+
 jablko = Jablko()
 jablka = pygame.sprite.Group()
 jablka.add(jablko)
@@ -35,6 +42,8 @@ while gra_dziala:
             gra_dziala = False
 
     ekran.blit(tlo, (0,0))
+
+    ekran.blit(waz.obraz, waz.rect)
     
     for jablko in jablka:
         ekran.blit(jablko.obraz, jablko.rect)
